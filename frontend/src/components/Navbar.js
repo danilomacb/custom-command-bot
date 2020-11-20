1d12481928900afa4b4c0a1788d7c5ea14282195
danilomacb <danilo.bakun@gmail.com>
2020-11-20 07:11:35 -0300

    logout

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,31 +5,47 @@ import "../styles/navbar.scss";
 import { UserContext } from "../Context/UserContext";
 
 function Navbar() {
-  const { user } = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
 
   return (
     <nav>
       <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        {user ? (
-          <>
-            <li id="user">
-              <img
-                src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`}
-                alt="user profile"
-              />
-              {user.username}
-            </li>
-          </>
-        ) : (
+        <div id="left">
           <li>
-            <a href="https://discord.com/api/oauth2/authorize?client_id=778216582329270302&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&response_type=token&scope=identify">
-              Login
-            </a>
+            <Link to="/">Home</Link>
           </li>
-        )}
+        </div>
+        <div id="right">
+          {user ? (
+            <>
+              <li id="user">
+                <img
+                  src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`}
+                  alt="user profile"
+                />
+                {user.username}
+              </li>
+              <li>
+                <button
+                  onClick={() => {
+                    localStorage.removeItem("token");
+                    localStorage.removeItem("tokenType");
+
+                    setUser(null);
+                  }}
+                >
+                  Logout
+                </button>
+              </li>
+            </>
+          ) : (
+            <li>
+              <a href="https://discord.com/api/oauth2/authorize?client_id=778216582329270302&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&response_type=token&scope=identify">
+                <button>Login</button>
+              </a>
+            </li>
+          )}
+        </div>
       </ul>
     </nav>
   );
