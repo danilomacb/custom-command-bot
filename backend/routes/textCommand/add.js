dd85e84cd50dc3de327131317c58c1b303c184f3
danilomacb <danilo.bakun@gmail.com>
2020-11-30 09:54:43 -0300

    backend: add text command route protected

--- backend/routes/textCommand/add.js
+++ backend/routes/textCommand/add.js
@@ -1,17 +1,36 @@
+const getDiscordUserReq = require("../discordApi/util/getDiscordUserReq");
 const DiscordServer = require("../../models/DiscordServer");
 const successHandler = require("../util/successHandler");
 const errorHandler = require("../util/errorHandler");
 
 async function add(req, res) {
+  const { authorization } = req.headers;
   const { tag, message } = req.body;
+  const { discordServerId } = req.params;
+
+  let user;
+  try {
+    user = await getDiscordUserReq(authorization);
+  } catch (err) {
+    errorHandler(res, 500, `Fail to get user data with token: ${authorization}`, err);
+    return;
+  }
 
   let discordServer;
   try {
-    discordServer = await DiscordServer.findOne({
-      discordServerId: req.params.discordServerId,
-    });
+    discordServer = await DiscordServer.findOne({ discordServerId });
   } catch (err) {
     errorHandler(res, 500, `Fail to add text command: ${tag}`, err);
+    return;
+  }
+
+  const userFound = discordServer.superAdms.find((superAdm) => superAdm.discordUserId === user.data.id);
+  if (!userFound) {
+    errorHandler(
+      res,
+      401,
+      `User with token: ${authorization} doesn't have permission to add text command in discord server with id: ${discordServerId}`
+    );
     return;
   }
 
