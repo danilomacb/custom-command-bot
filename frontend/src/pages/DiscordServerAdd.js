28e2ac1bcb9c8d1d451a552b19cb60e315f0298e
danilomacb <danilo.bakun@gmail.com>
2020-12-06 08:18:25 -0300

    frontend: Link added on DiscordServerAdd to back to DiscordServer

--- frontend/src/pages/DiscordServerAdd.js
+++ frontend/src/pages/DiscordServerAdd.js
@@ -1,9 +1,12 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 import "../styles/tabs.scss";
 import TextForm from "../components/TextForm";
 
 function DiscordServer({ match, discordServer }) {
+  const { discordServerId } = match.params;
+
   const [tab, setTab] = useState("text");
 
   function changeTab(t) {
@@ -16,14 +19,11 @@ function DiscordServer({ match, discordServer }) {
     <>
       <h1>{discordServer.name}</h1>
       <div className="tabs">
+        <Link to={`/discord-server/${discordServerId}`}>Back</Link>
         <button onClick={() => changeTab("text")}>Text</button>
         <button onClick={() => changeTab("image")}>Image</button>
       </div>
-      {tab === "text" ? (
-        <TextForm discordServerId={match.params.discordServerId} />
-      ) : (
-        <h1>ImageForm</h1>
-      )}
+      {tab === "text" ? <TextForm discordServerId={discordServerId} /> : <h1>ImageForm</h1>}
     </>
   );
 }
