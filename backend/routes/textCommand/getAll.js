7f00e3ded39376088b9b17675f3d9b8cb87631e5
danilomacb <danilo.bakun@gmail.com>
2020-12-11 07:45:31 -0300

    backend: using success and error handler on get all text commands

--- backend/routes/textCommand/getAll.js
+++ backend/routes/textCommand/getAll.js
@@ -1,12 +1,24 @@
-const { response } = require("express");
 const DiscordServer = require("../../models/DiscordServer");
+const successHandler = require("../../util/successHandler");
+const errorHandler = require("../../util/errorHandler");
 
 async function getAll(req, res) {
   const { discordServerId } = req.params;
 
-  const discordServer = await DiscordServer.findOne({ discordServerId });
+  try {
+    const discordServer = await DiscordServer.findOne({ discordServerId });
 
-  res.status(200).json({textCommands: discordServer.textCommands});
+    successHandler(res, 200, `All text commands listed, data: {discordServer: ${discordServer}}`, {
+      textCommands: discordServer.textCommands,
+    });
+  } catch (err) {
+    errorHandler(
+      res,
+      500,
+      `Fail to list all text commands, data: {discordServer: ${discordServer}}`,
+      err
+    );
+  }
 }
 
 module.exports = getAll;
