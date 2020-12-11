f4b1d4aa004731c45f72b1e2d2317325708ad6cc
danilomacb <danilo.bakun@gmail.com>
2020-12-11 07:44:01 -0300

    backend: members removed from add discord server log

--- backend/routes/discordServer/add.js
+++ backend/routes/discordServer/add.js
@@ -15,14 +15,14 @@ async function add(req, res) {
     successHandler(
       res,
       201,
-      `New discord server registered on the database, data: {name: ${name}, discordServerId: ${discordServerId}, members: ${members}}`
+      `New discord server registered on the database, data: {name: ${name}, discordServerId: ${discordServerId}}`
     );
     return;
   } catch (err) {
     errorHandler(
       res,
       500,
-      `Fail to register a new discord server on the database, data: {name: ${name}, discordServerId: ${discordServerId}}, members: ${members}`,
+      `Fail to register a new discord server on the database, data: {name: ${name}, discordServerId: ${discordServerId}}`,
       err
     );
     return;
