2ec2a55800632bfbb391243037239465fc61859f
danilomacb <danilo.bakun@gmail.com>
2020-12-14 08:40:19 -0300

    frontend: discord server add route, CheckAdmDiscordServer and ProtectDiscordServerAdd removed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,14 @@ import { Route, Router } from "react-router-dom";
 import history from "./history";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import CheckAdmDiscordServer from "./components/CheckAdmDiscordServer";
-import ProtectDiscordServerAdd from "./components/ProtectDiscordServerAdd";
+import DiscordServer from "./pages/DiscordServer";
 
 function App() {
   return (
     <Router history={history}>
       <Navbar />
       <Route exact path="/" component={Home} />
-      <Route exact path="/discord-server/:discordServerId/" component={CheckAdmDiscordServer} />
-      <Route
-        exact
-        path="/discord-server/:discordServerId/add"
-        component={ProtectDiscordServerAdd}
-      />
+      <Route exact path="/discord-server/:discordServerId/" component={DiscordServer} />
     </Router>
   );
 }
