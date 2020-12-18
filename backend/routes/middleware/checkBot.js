bb9b7e4c69ac088a3767d2c4650af089ca287617
danilomacb <danilo.bakun@gmail.com>
2020-12-18 08:32:32 -0300

    backend: checkBot created to give access to some routes for only the bot

---
+++ backend/routes/middleware/checkBot.js
@@ -0,0 +1,34 @@
+const axios = require("axios");
+
+const errorHandler = require("../../util/errorHandler");
+
+async function checkBot(req, res, next) {
+  const { authorization } = req.headers;
+  const { name, discordServerId, members } = req.body;
+
+  let bot;
+  try {
+    bot = await axios.get("https://discord.com/api/oauth2/applications/@me", {
+      headers: { authorization },
+    });
+  } catch (err) {
+    errorHandler(
+      res,
+      500,
+      `Fail to get bot data from discord api, data: {discordServerName: ${name}, discordServerId: ${discordServerId}}`,
+      err
+    );
+  }
+
+  if (bot.data.id !== "777841418483662868") {
+    errorHandler(
+      res,
+      500,
+      `Permission denied, data: {discordServerName: ${name}, discordServerId: ${discordServerId}}`
+    );
+  }
+
+  next();
+}
+
+module.exports = checkBot;
