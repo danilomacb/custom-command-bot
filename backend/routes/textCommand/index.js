2c9b7fc232af50f17693fb8253d15b27cacbcff9
danilomacb <danilo.bakun@gmail.com>
2020-12-21 12:28:30 -0300

    backend: update text command route created

--- backend/routes/textCommand/index.js
+++ backend/routes/textCommand/index.js
@@ -3,10 +3,12 @@ const express = require("express");
 const checkToken = require("../middleware/checkToken");
 const add = require("./add");
 const getAll = require("./getAll");
+const update = require("./update");
 
 const router = express.Router();
 
 router.post("/:discordServerId/add", checkToken, add);
 router.get("/:discordServerId/get-all", checkToken, getAll);
+router.put("/:discordServerId/update/:textCommandId", checkToken, update);
 
 module.exports = router;
---
+++ backend/routes/textCommand/update.js
@@ -0,0 +1,64 @@
+const successHandler = require("../../util/successHandler");
+const errorHandler = require("../../util/errorHandler");
+
+async function update(req, res) {
+  const { member, discordServer } = res.locals;
+  const { textCommandId } = req.params;
+  const { tag, message } = req.body;
+
+  discordServer.textCommands.map((textCommand) => {
+    if (textCommand.id === textCommandId) {
+      if (member.superAdm || (member.adm && member.discordUserId === textCommand.discordUserId)) {
+        textCommand.tag = tag;
+        textCommand.message = message;
+      } else {
+        errorHandler(
+          res,
+          401,
+          `Permission denied,
+memberUsername: ${member.discordUsername},
+memberDiscriminator: ${member.discordDiscriminator},
+memberId, ${member.discordUserId},
+discordServerName: ${discordServer.name},
+discordServerId: ${discordServer.discordServerId}`
+        );
+        return;
+      }
+    }
+  });
+
+  try {
+    await discordServer.save();
+
+    successHandler(
+      res,
+      200,
+      `Text command updated
+tag: ${tag},
+message: ${message},
+textCommandId: ${textCommandId}
+memberUsername: ${member.discordUsername},
+memberDiscriminator: ${member.discordDiscriminator},
+memberId, ${member.discordUserId},
+discordServerName: ${discordServer.name},
+discordServerId: ${discordServer.discordServerId}`
+    );
+  } catch (err) {
+    errorHandler(
+      res,
+      500,
+      `Error on update text command, save failed
+tag: ${tag},
+message: ${message},
+textCommandId: ${textCommandId}
+memberUsername: ${member.discordUsername},
+memberDiscriminator: ${member.discordDiscriminator},
+memberId, ${member.discordUserId},
+discordServerName: ${discordServer.name},
+discordServerId: ${discordServer.discordServerId}`,
+      err
+    );
+  }
+}
+
+module.exports = update;
