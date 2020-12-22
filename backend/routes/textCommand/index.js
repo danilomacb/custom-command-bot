26cb6c29fc669e4fc19e1abbc556ace923713733
danilomacb <danilo.bakun@gmail.com>
2020-12-22 09:59:46 -0300

    backend: remove text command route created

--- backend/routes/textCommand/index.js
+++ backend/routes/textCommand/index.js
@@ -4,11 +4,13 @@ const checkToken = require("../middleware/checkToken");
 const add = require("./add");
 const getAll = require("./getAll");
 const update = require("./update");
+const remove = require("./remove");
 
 const router = express.Router();
 
 router.post("/:discordServerId/add", checkToken, add);
 router.get("/:discordServerId/get-all", checkToken, getAll);
 router.put("/:discordServerId/update/:textCommandId", checkToken, update);
+router.delete("/:discordServerId/remove/:textCommandId", checkToken, remove);
 
 module.exports = router;
---
+++ backend/routes/textCommand/remove.js
@@ -0,0 +1,63 @@
+const successHandler = require("../../util/successHandler");
+const errorHandler = require("../../util/errorHandler");
+
+async function remove(req, res) {
+  const { member, discordServer } = res.locals;
+  const { textCommandId } = req.params;
+
+  let command, index;
+  discordServer.textCommands.map((textCommand, textCommandIndex) => {
+    if (textCommand.id === textCommandId) {
+      command = textCommand;
+      index = textCommandIndex;
+      return;
+    }
+  });
+
+  if (member.superAdm || (member.adm && member.discordUserId === command.discordUserId)) {
+    discordServer.textCommands.splice(1, index);
+  } else {
+    errorHandler(
+      res,
+      401,
+      `Permission denied,
+memberUsername: ${member.discordUsername},
+memberDiscriminator: ${member.discordDiscriminator},
+memberId, ${member.discordUserId},
+discordServerName: ${discordServer.name},
+discordServerId: ${discordServer.discordServerId}`
+    );
+    return;
+  }
+
+  try {
+    await discordServer.save();
+
+    successHandler(
+      res,
+      200,
+      `Text command removed,
+  textCommandId: ${textCommandId}
+  memberUsername: ${member.discordUsername},
+  memberDiscriminator: ${member.discordDiscriminator},
+  memberId, ${member.discordUserId},
+  discordServerName: ${discordServer.name},
+  discordServerId: ${discordServer.discordServerId}`
+    );
+  } catch (err) {
+    errorHandler(
+      res,
+      500,
+      `Error on remove text command, save failed
+  textCommandId: ${textCommandId}
+  memberUsername: ${member.discordUsername},
+  memberDiscriminator: ${member.discordDiscriminator},
+  memberId, ${member.discordUserId},
+  discordServerName: ${discordServer.name},
+  discordServerId: ${discordServer.discordServerId}`,
+      err
+    );
+  }
+}
+
+module.exports = remove;
