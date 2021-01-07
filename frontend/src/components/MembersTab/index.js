4898342a060d140bf884c48ccec44e02092781c6
Danilo <danilo.bakun@gmail.com>
2021-01-07 19:40:18 -0300

    frontend: creating few styles for the members tab

--- frontend/src/components/MembersTab/index.js
+++ frontend/src/components/MembersTab/index.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 
+import "../../styles/membersTab.scss";
 import { listAllMembers } from "../../services/MemberService";
 
 function MembersTab({ discordServer }) {
@@ -9,20 +10,28 @@ function MembersTab({ discordServer }) {
     listAllMembers(discordServer.discordServerId).then((res) => setMembersList(res));
   }, [discordServer.discordServerId]);
 
+  function promote() {
+    console.log("promoted");
+  }
+
   if (membersList) {
     return (
       <>
         {membersList.map((member) => (
-          <div key={member.discordUserId}>
+          <div key={member.discordUserId} className="member">
             {member.discordUserAvatar ? (
               <img
                 src={`https://cdn.discordapp.com/avatars/${member.discordUserId}/${member.discordUserAvatar}.png`}
                 alt="avatar"
               />
             ) : null}
-            <div key={member.discordUserId}>
+            <div>
               {member.discordUserUsername}#{member.discordUserDiscriminator}
             </div>
+            <input type="checkbox" name="superAdm" checked={member.superAdm} onChange={promote} />
+            <label htmlFor="superAdm">Super Adm</label>
+            <input type="checkbox" name="adm" checked={member.adm} onChange={promote} />
+            <label htmlFor="adm">Adm</label>
           </div>
         ))}
       </>
