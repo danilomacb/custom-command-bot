ede0e7cc63b21d1e49141c24fad0b5367836c082
danilomacb <danilo.bakun@gmail.com>
2021-01-11 11:01:03 -0300

    frontend: listOneMember removed from MemberService

--- frontend/src/services/MemberService.js
+++ frontend/src/services/MemberService.js
@@ -1,35 +1,3 @@
-export async function listOneMember(discordServerId) {
-  let res;
-  try {
-    res = await fetch(`http://localhost:3001/member/${discordServerId}/list-one`, {
-      method: "GET",
-      headers: {
-        authorization: `${localStorage.tokenType} ${localStorage.token}`,
-      },
-    });
-  } catch (err) {
-    alert("Fail to list discord server member");
-    console.error("Fail to list discord server member\n", err);
-    return;
-  }
-
-  let jsonRes;
-  try {
-    jsonRes = await res.json();
-  } catch (err) {
-    alert("Fail to list discord server member");
-    console.error("Fail to convert response to json\n", err);
-    return;
-  }
-
-  if (!res.ok) {
-    alert(jsonRes.message);
-    return;
-  }
-
-  return jsonRes.data.members;
-}
-
 export async function listAllMembers(discordServerId) {
   let res;
   try {
