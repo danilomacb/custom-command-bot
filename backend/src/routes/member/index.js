025caf8a07efdb10e7579c934c9456d25e107072
danilomacb <danilo.bakun@gmail.com>
2021-01-11 11:00:42 -0300

    backend: list one member route removed

--- backend/src/routes/member/index.js
+++ backend/src/routes/member/index.js
@@ -4,7 +4,6 @@ const checkBot = require("../middleware/checkBot");
 const checkToken = require("../middleware/checkToken");
 const add = require("./add");
 const listAll = require("./listAll");
-const listOne = require("./listOne");
 const update = require("./update");
 const remove = require("./remove");
 
@@ -12,7 +11,6 @@ const router = express.Router();
 
 router.post("/:discordServerId/add", checkBot, add);
 router.get("/:discordServerId/list-all", checkToken, listAll);
-router.get("/:discordServerId/list-one", checkToken, listOne);
 router.put("/:discordServerId/update/:discordUserIdToUpdate", checkToken, update);
 router.delete("/:discordServerId/remove/:discordUserId", checkBot, remove);
 
