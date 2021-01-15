a1f9e2ffb67ed02e133197e3a927e90948881b40
danilomacb <danilo.bakun@gmail.com>
2021-01-15 07:39:09 -0300

    bot: minor fix on list all text commands request

--- bot/src/commands.js
+++ bot/src/commands.js
@@ -5,7 +5,7 @@ const { prefix } = require("../config.json");
 async function commands(message) {
   const res = await axios.get(`${process.env.BACKEND_LINK_DEV}/text/${message.guild.id}/list-all`);
 
-  textCommandFound = res.data.textCommands.find(
+  textCommandFound = res.data.data.textCommands.find(
     (textCommand) => `${prefix}${textCommand.tag}` === message.content
   );
 
